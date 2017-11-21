bef5ebaa30ea5f134be4ee2f9d753a46b53a47df
Oleg Medvedev <omdv@fastmail.com>
2017-11-21 05:18:39 -0600

    removed oauth

--- App/Config/OAuth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	coinbase: {
-		client_id: '13ddf49a5d5c449c27f786bbb97851203ac600fb5476b336743db71b92cb9604',
-		client_secret: '41479e03888da205439df0bf8874513ea08e65259c08f1178abe16bd0e19c29d',
-		redirect_uri: 'cryptofolio://oauth',
-		scopes: 'wallet:accounts:read,wallet:transactions:read'
-	}
-};
